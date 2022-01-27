5a2ce5b0e92926187c31e21e78109beeaac1af20
seong <tjddn3465@onandon.co.kr>
2022-01-27 11:18:11 +0900

    손지현차장 머지

--- html/sub05/050102.php
+++ html/sub05/050102.php
@@ -16,31 +16,47 @@ echo "<script>alert('로그인이 필요한 페이지 입니다.');history.back();</script>";
 		<td class="cell03 last pdt10">ㆍ이*협(4세 이*윤 父)   <br>
 		ㆍ홍*지(1세 정*윤 母)</td>
 	</tr> -->
+    <tr>
+        <td align="center" class="cell07_r" style="border-bottom: 1px solid #a9c961;">구분</td>
+        <td align="center" class="cell07_r" style="border-bottom: 1px solid #a9c961;">위원</td>
+    </tr>
+    <tr>
+        <td align="center" class="cell07_r" style="border-bottom: 1px solid #a9c961;">운영 위원장</td>
+        <td align="center" class="cell03 last pdt10" style="border-bottom: 1px solid #a9c961;">
+            ㆍ박*민(2세 박*하 父)
+        </td>
+    </tr>
+    <tr>
+        <td align="center" class="cell07_r" style="border-bottom: 1px solid #a9c961;">통합 운영 위원</td>
+        <td align="center" class="cell03 last pdt10" style="border-bottom: 1px solid #a9c961;">
+            ㆍ이*진(3세 정*진 母)<br>
+            ㆍ최*진(0세 김*은 母)
+        </td>
+    </tr>
 	<tr>
-		<td align="center" class="cell07_r">운영 위원</td>
-		<td class="cell03 last pdt10">
-		ㆍ최*진(0세 김*은 母)<br>
-		ㆍ이*경(1세 김*현 母)<br>
-		ㆍ박*민(2세 박*하 父)<br>
-		ㆍ이*진(3세 정*진 母)<br>
-		ㆍ장*석(4세 장*재 父)<br>
-		ㆍ홍*옥(4세 홍*헌 父)<br>
-		ㆍ이*협(5세 이*윤 父) </td>
+		<td align="center" class="cell07_r" style="border-bottom: 1px solid #a9c961;">운영 위원</td>
+		<td align="center" class="cell03 last pdt10" style="border-bottom: 1px solid #a9c961;">
+            ㆍ이*경(1세 김*현 母)<br>
+            ㆍ박*원(3세 박*율 / 2세 박*민 父)<br>
+		    ㆍ장*석(4세 장*재 父)<br>
+		    ㆍ홍*지(2세 정*윤/5세 정*호 母)<br>
+		</td>
 	</tr>   
 	<tr>
-		<td align="center" class="cell07_r">어린이집 운영위원</td>
-		<td class="cell03 last pdt10">
-		ㆍ원장 : 김지현<br>
-		ㆍ원감 : 이순배<br>
-		ㆍ교사 : 쉬린징(선임)</td>
+		<td align="center" class="cell07_r" style="border-bottom: 1px solid #a9c961;">어린이집 운영위원</td>
+		<td align="center" class="cell03 last pdt10" style="border-bottom: 1px solid #a9c961;">
+		    ㆍ원장 : 김지현<br>
+		    ㆍ원감 : 이순배<br>
+		    ㆍ교사 : 쉬린징(선임)
+        </td>
 	</tr>
 	<tr>
-		<td align="center" class="cell07_r">직장업체 운영위원</td>
-		<td class="cell03 last pdt10">ㆍ김남경 (사무처 운영지원과)</td>
+		<td align="center" class="cell07_r" style="border-bottom: 1px solid #a9c961;">직장업체 운영위원</td>
+		<td align="center" class="cell03 last pdt10" style="border-bottom: 1px solid #a9c961;">ㆍ김남경 (사무처 운영지원과)</td>
 	</tr>
 	<tr>
-		<td align="center" class="cell07_r">위탁체 운영위원</td>
-		<td class="cell03 last pdt10">ㆍ이화여대 유아교육과 교수 </td>
+		<td align="center" class="cell07_r" style="border-bottom: 1px solid #a9c961;">위탁체 운영위원</td>
+		<td align="center" class="cell03 last pdt10" style="border-bottom: 1px solid #a9c961;">ㆍ이화여대 유아교육과 교수 </td>
 	</tr>
 </table>
 <!---//본문내용--->
